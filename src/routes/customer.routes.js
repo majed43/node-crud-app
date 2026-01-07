342b0415690c6e0e5e7b1e42f9c4fa8b2d8209ad
Majed <majd121289@gmail.com>
2026-01-07 17:00:06 +0300

    fix main rout

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -2,6 +2,7 @@ const express=require("express");
 const router=express.Router();
 const Customer = require("../model/customer.model.js");
 const customerController=require("../controllers/customer.controller.js");
+router.get("/",customerController.renderHome)
 router.get("/home",customerController.renderHome);
 router.get("/add-customers",customerController.renderAdd);
 router.get("/view/:id",customerController.viewDetails);
